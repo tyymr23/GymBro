58061bafd8d4b346423a82b0715ae6b3d02c4b06
tyymr23 <tmrobertson2323@gmail.com>
2023-05-27 15:20:53 -0400

    GymBro - fix: Detail paragraph

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -31,10 +31,9 @@ const Detail = ({ exerciseDetail }) => {
           {name}
         </Typography>
         <Typography sx={{ fontSize: { lg: '24px', xs: '18px' } }} color="#4F4C4C">
-          Exercises keep you strong.{' '}
-          <span style={{ textTransform: 'capitalize' }}>{name}</span> bup is one
-          of the best <br /> exercises to target your {target}. It will help you improve your{' '}
-          <br /> mood and gain energy.
+          Exercises keep you strong. {name} {' '}
+          is one of the best exercises to target your {target}. It will help you improve your 
+          mood and gain energy.
         </Typography>
         {extraDetail?.map((item) => (
           <Stack key={item.name} direction="row" gap="24px" alignItems="center">
